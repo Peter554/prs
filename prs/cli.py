3c379073004fb0af77283676eebb7c538048145a
Peter Byfield <peter.byfield@octoenergy.com>
2024-02-11 14:57:52 +0100

    Hide reviews by default

--- prs/cli.py
+++ prs/cli.py
@@ -9,63 +9,72 @@ from rich.table import Table
 from . import github
 
 cli = typer.Typer()
+console = Console()
 
 
 @cli.command()
-def main() -> None:
-    asyncio.run(main_async())
+def main(
+    mine: bool = True, review_requests: bool = True, reviewed: bool = False
+) -> None:
+    asyncio.run(main_async(mine, review_requests, reviewed))
 
 
-async def main_async() -> None:
+async def main_async(mine: bool, review_requests: bool, reviewed: bool) -> None:
     two_weeks_ago = datetime.datetime.now(datetime.UTC) - datetime.timedelta(weeks=2)
 
+    tasks: dict[str, asyncio.Task[list[github.PullRequest]]] = {}
     async with asyncio.TaskGroup() as tg:
-        prs_where_i_am_author_task = tg.create_task(
-            github.get_pull_requests("author:peter554")
-        )
-        prs_where_i_am_assigned_task = tg.create_task(
-            github.get_pull_requests("assignee:peter554")
-        )
-        prs_where_my_review_has_been_requested_task = tg.create_task(
-            github.get_pull_requests("user-review-requested:peter554")
-        )
-        prs_i_have_reviewed_task = tg.create_task(
-            github.get_pull_requests(
-                f"reviewed-by:peter554 updated:>{two_weeks_ago.date()}"
+        if mine:
+            tasks["prs_where_i_am_author_task"] = tg.create_task(
+                github.get_pull_requests("author:peter554")
             )
+            tasks["prs_where_i_am_assigned_task"] = tg.create_task(
+                github.get_pull_requests("assignee:peter554")
+            )
+        if review_requests:
+            tasks["prs_where_my_review_has_been_requested_task"] = tg.create_task(
+                github.get_pull_requests("user-review-requested:peter554")
+            )
+        if reviewed:
+            tasks["prs_i_have_reviewed_task"] = tg.create_task(
+                github.get_pull_requests(
+                    f"reviewed-by:peter554 updated:>{two_weeks_ago.date()}"
+                )
+            )
+
+    if mine:
+        my_prs = set(tasks["prs_where_i_am_author_task"].result()) | set(
+            tasks["prs_where_i_am_assigned_task"].result()
+        )
+        _render_prs(
+            "My PRs",
+            _sort_and_take(my_prs, 10),
         )
 
-    my_prs = set(prs_where_i_am_author_task.result()) | set(
-        prs_where_i_am_assigned_task.result()
-    )
-    prs_where_my_review_has_been_requested = set(
-        prs_where_my_review_has_been_requested_task.result()
-    )
-    prs_i_have_reviewed = set(prs_i_have_reviewed_task.result()) - my_prs
+    if review_requests:
+        prs_where_my_review_has_been_requested = set(
+            tasks["prs_where_my_review_has_been_requested_task"].result()
+        )
+        _render_prs(
+            "PRs where my review is requested",
+            _sort_and_take(prs_where_my_review_has_been_requested, 10),
+        )
 
-    console = Console()
-    _render_prs(
-        console,
-        "My PRs",
-        _sort_and_take(my_prs, 10),
-    )
-    _render_prs(
-        console,
-        "PRs where my review is requested",
-        _sort_and_take(prs_where_my_review_has_been_requested, 10),
-    )
-    _render_prs(
-        console,
-        "PRs I have reviewed",
-        _sort_and_take(prs_i_have_reviewed, 10),
-    )
+    if reviewed:
+        prs_i_have_reviewed = set(tasks["prs_i_have_reviewed_task"].result())
+        if mine:
+            prs_i_have_reviewed -= my_prs
+        _render_prs(
+            "PRs I have reviewed",
+            _sort_and_take(prs_i_have_reviewed, 10),
+        )
 
 
 def _sort_and_take(prs: set[github.PullRequest], n: int) -> list[github.PullRequest]:
     return list(sorted(prs, key=lambda pr: pr.updated_at, reverse=True))[:n]
 
 
-def _render_prs(console: Console, title: str, prs: list[github.PullRequest]) -> None:
+def _render_prs(title: str, prs: list[github.PullRequest]) -> None:
     table = Table(title=title, expand=True)
 
     table.add_column("PR")
